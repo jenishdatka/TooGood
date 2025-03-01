9f74d11660f7f4f9f7d922ce8d6d94f5f16cddb2
jenishdatka <jenishdatka@gmail.com>
2025-03-01 15:26:37 +0600

    added detail product page

--- core/main/urls.py
+++ core/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index')
+    path('', views.index_view, name='index'),
+    path('product/<int:product_id>', views.product_detail_view, name='product_detail')
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from unicodedata import category
+
 from .models import Product
 
 def index_view(request):
     products = Product.objects.filter(is_active=True)
 
-    return render(request=request, template_name='main/index.html', context={'products': products})
+    return render(request,template_name='main/index.html', context={'products': products})
+
+def product_detail_view(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    similar_products = Product.objects.filter(category=product.category).execute(id=product.id)
+
+    return render(request=request,
+                  template_name='main/product_detail.html',
+                  context = {'product':product,'similar_products':similar_products})
