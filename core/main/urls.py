13ad4cbfd7871c0778d76096d2f4ce7d0dc26338
jenishdatka <jenishdatka@gmail.com>
2025-03-06 15:49:15 +0600

    added product_update

--- core/main/urls.py
+++ core/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='index'),
-    path('product/<int:product_id>', views.product_detail_view, name='product_detail')
+    path('product/<int:product_id>', views.product_detail_view, name='product_detail'),
+    path('product/create/', views.product_create_view, name='product_create'),
+    path('product/update/<int:product_id>', views.product_update_view, name='product_update')
 ]
--- core/main/views.py
+++ core/main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from unicodedata import category
-
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from .forms import ProductCreateForm, ProductUpdateForm
 
 def index_view(request):
     products = Product.objects.filter(is_active=True)
@@ -10,9 +10,32 @@ def index_view(request):
 
 def product_detail_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
+    product_update_form= ProductUpdateForm()
 
     similar_products = Product.objects.filter(category=product.category).exclude(id=product.id)
 
     return render(request=request,
                   template_name='main/product_detail.html',
-                  context = {'product':product,'similar_products':similar_products})
+                  context = {'product':product,'similar_products':similar_products,
+                             "product_update_form": product_update_form})
+
+def product_create_view(request):
+    if request.method == 'POST':
+        form = ProductCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            product_object = form.save(commit=False)
+            product_object.user = request.user
+            product_object.save()
+
+            messages.success(request, 'Успешно создано!')
+            return redirect('index')
+    else:
+        form = ProductCreateForm()
+    return render(request, 'main/product_create.html', context={'form': form})
+
+def product_update_view(request, product_id):
+    return render(
+        request=request,
+        template_name='main/product_update_modal.html'
+    )
+
